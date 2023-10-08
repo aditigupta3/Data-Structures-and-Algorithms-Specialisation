3a5ff4183531e49458d7b2f9393d87a7193bea61
aditi_gupta1 <aditi.gupta@apna.co>
2023-10-08 15:57:05 +0530

    week 1 programming assignment

---
+++ Algorithmic Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -0,0 +1,30 @@
+# python3
+
+
+def max_pairwise_product(numbers):
+    """
+    Since we are only interested in the maximum pairwise product and all numbers are positive,
+    we just need to get the top 2 integers and multiply them to get the result.
+    :param numbers:
+    :return:
+    """
+    if numbers[0] > numbers[1]:
+        small = numbers[1]
+        large = numbers[0]
+    else:
+        large = numbers[1]
+        small = numbers[0]
+    for elem in numbers[2:]:
+        if (elem > small) and (elem <= large):
+            small = elem
+        elif elem > large:
+            small = large
+            large = elem
+    return small*large
+
+
+if __name__ == '__main__':
+    """Press Ctrl + D on mac to specify end of input on mac terminal"""
+    input_n = input()
+    input_numbers = [int(x) for x in input_n.split()]
+    print(max_pairwise_product(input_numbers))
